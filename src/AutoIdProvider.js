f2eb5b8632f268474e53d64c07146be5c6f3e2a0
Brian Beck <exogen@gmail.com>
2020-02-04 15:47:52 -0800

    Document AutoIdProvider in comments

--- src/AutoIdProvider.js
+++ src/AutoIdProvider.js
@@ -3,8 +3,16 @@ import PropTypes from 'prop-types';
 
 let globalId = 0;
 
+// By default, if no AutoIdProvider is present, this `globalId` is the ID
+// counter. This will work fine for browser-only apps. For SSR support, use
+// AutoIdProvider, which will have a scoped counter per request.
 const Context = React.createContext(() => `svg-id-${globalId++}`);
 
+/**
+ * Provide auto generated IDs for the `useId` hook, with support for a custom
+ * ID generator. Using this will ensure that each server-side request that
+ * renders your app has their own ID counter that starts at 0.
+ */
 export default function AutoIdProvider({ children, getId: customGetId }) {
   const nextId = useRef(0);
 
